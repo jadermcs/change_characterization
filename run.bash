bec75fa414af45898d815a6697366acf45c99f4d
Jader <jader.martins@list.lu>
2025-01-24 09:33:25 +0100

    update

--- run.bash
+++ run.bash
@@ -4,7 +4,7 @@ for pole in "dimension" "relation" "orientation"; do
         echo "========$pole========"
         ctx="2048"
         if [[ "$model" = "DeepSeek-R1-Distill-Llama-8B-Q8_0.gguf" ]]; then
-            ctx="4096"
+            ctx="16384"
         fi
         
         for ((i = 0; i < 5; i++)); do
@@ -12,8 +12,6 @@ for pole in "dimension" "relation" "orientation"; do
             python prompt_generation.py data/$pole.csv few_shot.json $pole --style 0 --model $model --seed $i --ctx $ctx
             echo "cot $model"
             python prompt_generation.py data/$pole.csv few_shot.json $pole --style 1 --model $model --seed $i --ctx $ctx
-            # echo "few-shot $model"
-            # python prompt_generation.py data/$pole.csv few_shot.json $pole --style 1 --model $model --seed $i
         done
     done
 done
@@ -30,9 +28,5 @@ for pole in "dimension" "relation" "orientation"; do
         for ((i = 0; i < 5; i++)); do
             python compute_score.py data/$pole.csv $i output/1/ $pole $model
         done
-        # echo "few-shot $model"
-        # for ((i = 0; i < 5; i++)); do
-        #     python compute_score.py data/$pole.csv $i output/1_noreason/ $pole $model
-        # done
     done
 done
